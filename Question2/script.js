9ab197eb7b470e8512a0d8e85cfd33e5d1cabdb8
AkasDatta <012akasdatta@gmail.com>
2026-09-07 19:23:16 +0600

    added 2nd one

--- Question2/script.js
+++ Question2/script.js
@@ -2,6 +2,8 @@
 function getDayType(day) {
   const normalizedDay = day.toLowerCase();
   switch (normalizedDay) {
+    case monday;
+    case saturday;
     default:
       return "Invalid Day";
   }
