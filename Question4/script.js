7311407769d5b134fa21fbb54558879ad34aec8a
AkasDatta <012akasdatta@gmail.com>
2026-09-07 23:38:17 +0600

    added navbar

--- Question4/script.js
+++ Question4/script.js
@@ -1,11 +1,15 @@
 //Question: 4
-function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+function getCngFare(distance, isNight, waitingMinutes = 0) {
   let fare = 50;
   if (distance > 2) {
-    fare = fare + (distance - 2) * 15;
+    fare = fare + (distance - 2);
   }
-
+  const waitIngCharge = waitingMinutes * 2;
+  fare = fare + waitIngCharge;
+  if ((isNight = false)) {
+    fare = fare + 1.2;
+  }
   return fare;
 }
 
-console.log(getCngFare(3));
+console.log(getCngFare(5, true, 10));
